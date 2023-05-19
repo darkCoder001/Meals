f4c69c507de5406b923aceb248c3d301badcb011
Ishank Lalwani <ishanklalwani@gmail.com>
2023-05-19 16:14:37 +0530

    Animation added

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -7,15 +7,41 @@ import 'package:meals/widgets/category_grid_item.dart';
 import 'package:meals/models/category.dart';
 import 'package:meals/models/meal.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availableMeals});
 
    
 
    final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController=AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category){
-    final filteredMeals=availableMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    final filteredMeals=widget.availableMeals.where((meal) => meal.categories.contains(category.id)).toList();
     
     Navigator.of(context).push(MaterialPageRoute(
       builder: (ctx) => MealsScreen(
@@ -28,7 +54,9 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+
+    return AnimatedBuilder(
+      child: Padding(
       padding: const EdgeInsets.all(15),
       child: GridView(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -44,6 +72,15 @@ class CategoriesScreen extends StatelessWidget {
               },)    
         ],  
       ),
-    );
+    ),
+      animation: _animationController, 
+      builder: (context, child) => Padding(
+        padding: EdgeInsets.only(
+          top: 100-_animationController.value * 100,
+
+        ),
+        child: child,
+        )
+        )  ;
   }
 }
